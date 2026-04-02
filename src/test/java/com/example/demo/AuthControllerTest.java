587de1358f71d1fc639bcef50fc96b78de15c8c0
Zhanyl <123janyl456@gmail.com>
2026-04-02 22:03:05 +0200

    finished unit tests

---
+++ src/test/java/com/example/demo/AuthControllerTest.java
@@ -0,0 +1,58 @@
+package com.example.demo;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.when;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.example.demo.controller.AuthController;
+import com.example.demo.dto.UserRegistrationDto;
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@WebMvcTest(AuthController.class)
+
+class AuthControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    @MockBean
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @WithMockUser
+    void shouldRegisterData() throws Exception {
+        UserRegistrationDto userData = new UserRegistrationDto();
+        userData.setUsername("username1");
+        userData.setPassword("password1");
+
+        User mockUser = new User();
+
+        mockUser.setUsername("username1");
+        when(userService.registerUser(Mockito.any(UserRegistrationDto.class))).thenReturn(mockUser);
+
+        mockMvc.perform(post("/api/auth/register")
+            .with(csrf()) 
+
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(userData)))
+                .andExpect(status().isCreated())
+                .andExpect(content().string(org.hamcrest.Matchers.containsString("User created")));
+    }
+}
